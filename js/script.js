4893f23e9add3c805624811cf3e690c89b54106c
asakura <anphysik@gmail.com>
2020-03-25 15:36:12 +0200

    last

--- js/script.js
+++ js/script.js
@@ -7,10 +7,52 @@ $(document).ready(function(){
             this.style.animation='';
            }
            else{
-               var vasa = (index/7 + 0.3);
             this.style.animation=`navLinksFade .5s ease forwards ${index/7 + 0.3}s`
-            console.log(index/7 + 0.3); 
            }
           });
     });
-});
+
+ $('.pets-wrap').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    mobileFirst:true,
+    responsive: [
+       
+        {
+          breakpoint:1440,
+          settings: {
+            slidesToShow: 3
+          }  
+          },
+        {
+          breakpoint:1024,
+          settings: {
+            slidesToShow: 2
+          }  
+          },
+        {
+          breakpoint:768,
+          settings: {
+            slidesToShow: 1,
+            arrows: true
+          }  
+          },
+     
+        { 
+          breakpoint:425,
+          settings: {
+            slidesToShow: 1,
+            arrows:false
+          }
+        },
+        { 
+          breakpoint:320,
+          settings: {
+            slidesToShow: 1,
+            arrows:false
+          }
+        },
+    ]    
+  });
+  
+});new WOW().init();
